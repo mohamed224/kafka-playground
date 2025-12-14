07ba27024476df1008e5b0ef28e235c74d108b98
mohamed diaby <mdiaby00@gmail.com>
2025-12-14 21:17:51 +0100

    Initial commit

---
+++ consumer-service/src/test/java/fr/freetech4u/ReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.freetech4u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
